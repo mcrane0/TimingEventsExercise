4adc2599b988f7a4faacb0ae53daf5d8dc4685a2
Miles Crane <miles.crane@stu.barren.kyschools.us>
2023-05-08 09:58:09 -0500

    created index.html, style.css, and app.js with starting code

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+1. Use a setTimeout method to have "Hi" displayed as a p element/tag inside the div with the id of "first" 1 
+    second after the page loads.
+2. Use nested setTimeout methods to complete the following: Have the text of "One" displayed as a p element/tag 
+    inside the div with the id of "timeout-nesting" 2 seconds after the page loads. Also, below the "One" p 
+    element/tag, have the text of "Two" also displayed as a p element/tag inside the same div 3 seconds after 
+    the page loads.
+3a.  Use a setInterval method to display a counter in the console that starts at 1 and continues going up one 
+    number at a time every second.
+3b. Have the "STOP" button set up so that when it is pressed the setInterval method created in 3a will stop 
+    continuously running (HINT: Need to create a variable).
+*/
+
+const divFirst = document.querySelector('#first');
+
+
+// BONUS
+/*
+4. Using setInterval, display a countdown clock as a p element/tag inside the div with the id of "countdown". 
+    Have the countdown clock start at 2 minutes and count down. The countdown clock should display in the 
+    following format (Example: 2:00 to start and 0:00 at end). ALSO, when the countdown timer hits zero have 
+    it display a message saying… "TIME IS UP" instead of the clock (AKA 0:00).
+*/
+
